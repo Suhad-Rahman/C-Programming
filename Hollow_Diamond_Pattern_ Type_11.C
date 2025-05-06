f947eebced69bfa6555cc5eea6c50c56b81331b5
Suhad Rahman <suhadturan03@gmail.com>
2025-05-06 22:57:54 +0600

    Hollow Diamond Pattern | Language: C
    Visual-
    
    Enter N= 5
            * 
          *   * 
        *       * 
      *           * 
    *               * 
      *           * 
        *       * 
          *   * 
            *

---
+++ Hollow_Diamond_Pattern_ Type_11.C
@@ -0,0 +1,59 @@
+#include<stdio.h>
+int main(){
+int n, row, col;
+printf("Enter N= ");
+scanf("%d", &n);
+
+for(row=1; row<=n; row++){
+
+    for(col=1; col<=n-row; col++){
+
+        printf("  ");
+    }
+    for(col=1; col<=row; col++){
+
+        if(col==1){
+            printf("* ");
+        }
+        else{
+            printf("  ");
+        }
+        }
+    for(col=row-1; col>=1; col--){
+        if(col==1){
+            printf("* ");
+        }
+        else{
+            printf("  ");
+        }
+    }
+    printf("\n");
+}
+
+for(row=n-1; row>=1; row--){
+
+    for(col=1; col<=n-row; col++){
+        printf("  ");
+    }
+
+    for(col=1; col<=row; col++){
+        if(col==1){
+            printf("* ");
+        }
+        else{
+            printf("  ");
+        }
+    }
+    for(col=row-1; col>=1; col--){
+        if(col==1){
+            printf("* ");
+        }
+        else{
+            printf("  ");
+        }
+    }
+    printf("\n");
+
+}
+return 0;
+}
