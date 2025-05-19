1d671debe24e96d24f7b0e07b4928a8f53988d7d
Suhad Rahman <suhadturan03@gmail.com>
2025-05-20 01:26:01 +0600

    Matrix Addition with Random Values in C
    This C program takes user input for the dimensions of two 2D arrays (matrices), fills them with random integers (0–30), and performs matrix addition if their dimensions match. It displays all matrices (A, B, and the resulting C) in a formatted layout. If the dimensions do not match, it provides an informative message explaining why addition isn't possible.
    
    Key Features:
    
    Dynamic matrix size using Variable Length Arrays (VLAs)
    
    Random number generation using rand()
    
    Input validation for matrix addition
    
    Clean, readable output formatting

---
+++ 2D Array Random Addition.C
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+int a, b, c, d, e, f;
+printf("Enter the dimensions of the first array= ");
+scanf("%d %d", &a, &b);
+
+printf("Enter the dimensions of the second array= ");
+scanf("%d %d", &c, &d);
+int x[a][b],y[c][d], z[a][b];
+
+
+for(int i=0; i<a; i++){
+    for(int j=0; j<b; j++){
+        x[i][j]= rand()%31;
+    }
+}
+
+for(int i=0; i<c; i++){
+    for(int j=0; j<d; j++){
+        y[i][j]= rand()%31;
+    }
+}
+printf("\nMatrix A-->\n");
+for(int i=0; i<a; i++){
+    for(int j=0; j<b; j++){
+        printf("%3d ", x[i][j]);
+    }
+    printf("\n");
+}
+printf("\n\nMAtrix B-->\n");
+for(int i=0; i<c; i++){
+    for(int j=0; j<d; j++){
+        printf("%3d ", y[i][j]);
+    }
+    printf("\n");
+}
+if(a==c && b==d){
+printf("\nAddition\nMAtrix C-->\n");
+for(int i=0; i<c; i++){
+    for(int j=0; j<d; j++){
+    z[i][j]= x[i][j]+y[i][j];
+        printf("%3d ", z[i][j]);
+    }
+    printf("\n");
+    }
+}
+else{
+    printf("\nFor matrix addition to be defined, the two matrices you are adding must have the same dimensions.\n\n");
+}
+return 0;
+}
