5ee7fa06a9728a599e5aa524c162e57aca001950
Suhad Rahman <suhadturan03@gmail.com>
2025-05-20 23:37:10 +0600

    2D Array Random Addition.C

--- 2D Array Random Addition.C
+++ 2D Array Random Addition.C
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-int a, b, c, d, e, f;
+int a, b, c, d, i, j;
 printf("Enter the dimensions of the first array= ");
 scanf("%d %d", &a, &b);
 
@@ -10,35 +10,35 @@ scanf("%d %d", &c, &d);
 int x[a][b],y[c][d], z[a][b];
 
 
-for(int i=0; i<a; i++){
-    for(int j=0; j<b; j++){
+for(i=0; i<a; i++){
+    for(j=0; j<b; j++){
         x[i][j]= rand()%31;
     }
 }
 
-for(int i=0; i<c; i++){
-    for(int j=0; j<d; j++){
+for(i=0; i<c; i++){
+    for(j=0; j<d; j++){
         y[i][j]= rand()%31;
     }
 }
 printf("\nMatrix A-->\n");
-for(int i=0; i<a; i++){
-    for(int j=0; j<b; j++){
+for(i=0; i<a; i++){
+    for(j=0; j<b; j++){
         printf("%3d ", x[i][j]);
     }
     printf("\n");
 }
 printf("\n\nMAtrix B-->\n");
-for(int i=0; i<c; i++){
-    for(int j=0; j<d; j++){
+for(i=0; i<c; i++){
+    for(j=0; j<d; j++){
         printf("%3d ", y[i][j]);
     }
     printf("\n");
 }
 if(a==c && b==d){
 printf("\nAddition\nMAtrix C-->\n");
-for(int i=0; i<c; i++){
-    for(int j=0; j<d; j++){
+for(i=0; i<c; i++){
+    for(j=0; j<d; j++){
     z[i][j]= x[i][j]+y[i][j];
         printf("%3d ", z[i][j]);
     }
