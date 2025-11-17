2e1e2e983baee2c7dc7804bc00c0539a5ce5c873
Suhad Rahman <suhadturan03@gmail.com>
2025-11-17 20:13:43 +0600

    Print an Array of 10 Elements statically [DS]
    Data  Structure

---
+++ 1. Print an Array of 10 Elements statically..c
@@ -0,0 +1,21 @@
+//1. Print an Array of 10 Elements statically.
+
+#include<stdio.h>
+
+int main() {
+    
+    int arr[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    
+    printf("%d ", arr[0]);
+    printf("%d ", arr[1]);
+    printf("%d ", arr[2]);
+    printf("%d ", arr[3]);
+    printf("%d ", arr[4]);
+    printf("%d ", arr[5]);
+    printf("%d ", arr[6]);
+    printf("%d ", arr[7]);
+    printf("%d ", arr[8]);
+    printf("%d", arr[9]);
+    
+    return 0;
+}
